5fbf7e4bbfa58b8071e127c2387df4578279a019
Katerina <edrenina2@gmail.com>
2023-11-30 15:11:52 +0200

    /

---
+++ app.js
@@ -0,0 +1,7 @@
+let num1 = prompt ("Enter first number:");
+let num2 = prompt ("Enter second number:");
+let num3 = prompt ("Enter third number:");
+
+let arithmeticMean = (+(num1) + +(num2) + +(num3)) / 3;
+
+alert("The arithmetic mean of these three numbers is: " + arithmeticMean);
